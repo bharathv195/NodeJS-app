d3237ea9cd2e913126f1e4dd428f6dcf301057e0
bharathv195 <96901730+bharathv195@users.noreply.github.com>
2023-09-18 17:10:07 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -4,7 +4,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This application prints HelloWorld using NodeJS',
 	    'sonar.projectName': 'hello-world',
 	    'sonar.projectKey':'NodeJS',
-	    'sonar.login': 'squ_f2b4730acd404a033eb62400fb95064526d4f3e7',
+	    'sonar.login': '',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
